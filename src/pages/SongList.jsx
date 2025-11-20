a9e2c21dadb25bdde455299a55a8b17e5ca29c9a
tjsdn0520 <tjsdn0530@gmail.com>
2025-11-20 17:58:04 +0900

    💄Tailwind

--- src/pages/SongList.jsx
+++ src/pages/SongList.jsx
@@ -3,10 +3,12 @@ import SongCard from '../components/SongCard.jsx'
 
 export default function SongList({ songs }) {
   return (
-    <>
+    <div className='p-6'>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
       {songs.map(song => (
         <SongCard key={song.id} song={song} />
       ))}
-    </>
+      </div>
+    </div>
   )
 }
