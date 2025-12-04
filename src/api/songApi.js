68ca76179fbdc297449dea84286bf97c30d8ed00
tjsdn0520 <tjsdn0530@gmail.com>
2025-12-04 17:22:40 +0900

    🔖backend 연결

--- src/api/songApi.js
+++ src/api/songApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/songs',
+  baseURL: 'https://w25w11-mongodb-backend-1lsi.onrender.com/',
 })
 
 export const getSongList = async () => {
